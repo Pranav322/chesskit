f2378b6894af8609eaf99e7673a2d5cb8c8de7df
pranav322 <pranav88096@gmail.com>
2025-05-22 16:57:17 +0530

    fix: logs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -20,6 +20,12 @@ const firebaseConfig = {
 
 // DEBUG LOG — remove after testing
 console.log("API KEY at runtime:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("AUTH DOMAIN at runtime:", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+console.log("PROJECT ID at runtime:", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+console.log("STORAGE BUCKET at runtime:", process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
+console.log("MESSAGING SENDER ID at runtime:", process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID);
+console.log("APP ID at runtime:", process.env.NEXT_PUBLIC_FIREBASE_APP_ID);
+console.log("MEASUREMENT ID at runtime:", process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID);
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
